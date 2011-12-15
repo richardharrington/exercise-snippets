1c5803e63cbad9758b2696fcbe6caa6bb6a50804
Richard Harrington <harrington.richard@gmail.com>
2011-12-15 17:48:11 -0500

    added implement-queue-using-stack

---
+++ implement-queue-using-stack/implement-queue-using-stack.js
@@ -0,0 +1,31 @@
+// Add shift and unshift to the Array prototype, as if they didn't exist.
+
+// Give them new names, so that the standard versions
+// will still be there and we can compare them to our new ones.
+
+var transfer = function( arrayFrom, arrayTo, index ) {
+    var i = arrayFrom.length;
+    while (i > index) {
+        i--;
+        arrayTo.push( arrayFrom.pop() );
+    }
+};
+
+Array.prototype.newShift = function() {
+    var tempStack = [];
+    var result;
+    
+    transfer( this, tempStack, 1);
+    result = this.pop();
+    transfer( tempStack, this, 0);
+    return result;
+};
+
+
+Array.prototype.newUnshift = function( element ) {
+    var tempStack = [];
+    
+    transfer( this, tempStack, 0);
+    this.push( element );
+    transfer( tempStack, this, 0);
+};
